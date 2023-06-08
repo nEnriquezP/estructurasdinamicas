b0450ee392a34a33b112b958d383c49ba65c5893
nelsonenriquez <nenriquezp@miumg.edu.gt>
2023-06-07 20:00:29 -0600

    Creando  clase privada NODE, con sus atributos

--- src/estrucdinam.java
+++ src/estrucdinam.java
@@ -2,6 +2,20 @@
 
 public class estrucdinam {
 //Tarea Progra_3_Autor: Nelson Enríquez
+private Node head;
+    private Node tail;
+    private int size;
 
+    private class Node {
+        private int data;
+        private Node prev;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
 
 }
